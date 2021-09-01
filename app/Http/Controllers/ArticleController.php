c9be89c7a22f0b7326c52cf04ad5e15c1d8a9d85
Юрий Епихин <epih96@mail.ru>
2021-09-01 18:30:01 +0300

    Создание поста (CRUD)

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,9 +12,30 @@ class ArticleController extends Controller
         $articles = Article::paginate();
         return view('article.index', compact('articles'));
     }
+
     public function show($id)
     {
         $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
+    }
+
+    public function create()
+    {
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:1000'
+        ]);
+
+        $article = new Article();
+        $article->fill($data);
+        $article->save();
+        return redirect()
+            ->route('articles.index');
     }
 }
