3cbe441a5805d15ea021b74cf5a21fdb1732f7d5
Yuriy <epih96@mail.ru>
2021-09-03 14:06:01 +0300

    after switch to Ubuntu

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -18,7 +18,7 @@
 		<a href="articles/{{ $article->id }}"><h2>{{ $article->name }}</h2></a>
         <div>{{ Str::limit($article->body, 200) }}</div>
         <a href="{{ route('articles.edit', $article) }}">Редактировать статью</a><br>
-        <a href="{{ route('articles.destroy', $article) }}" data-confirm="Вы уверены?" data-method="delete" rel="nofollow">Удалить</a>
+        <a href="{{ route('articles.destroy', $article) }}" data-confirm="Вы уверены?" data-method="delete" rel="nofollow">Удалить</a><br>
 	@endforeach
 	<a href="{{ route('articles.create') }}">Создать статью</a>
 @endsection
